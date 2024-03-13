9ef12769dbf7d444363c723419cbd9411af7cf30
Cho Young Il <youngthedev@chunjae.co.kr>
2024-03-13 14:29:28 +0900

    style: 사용하지 않는 import문 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,17 +2,10 @@ import Head from "next/head";
 import { useTrackScroll } from "@/lib/trackScroll";
 import {
   NotionAPI,
-  NotionPageRenderer,
-  NotionDatabaseRenderer,
-  Database,
   NotionData,
-  Page,
 } from "@/lib/notionAPI";
 import Hero from "@/components/contents/Hero/Hero";
 import Soundbar from "@/components/layout/Soundbar/Soundbar";
-import Article from "@/components/contents/Article/Article";
-import Title from "@/components/text/Title/Title";
-import { useMemo } from "react";
 import NotionArticles from "@/components/contents/NotionArticles/NotionArticles";
 
 interface HomeProps {
