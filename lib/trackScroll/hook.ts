eee93120a58a81dce725dfcfec8d55d961759e41
Cho Young Il <youngthedev@chunjae.co.kr>
2024-03-15 17:09:58 +0900

    fix: 간헐적으로 track 정보가 뜨지 않는 현상

--- lib/trackScroll/hook.ts
+++ lib/trackScroll/hook.ts
@@ -2,6 +2,7 @@ import {useState, useEffect, useRef, useCallback, useMemo} from "react";
 
 const useTrackScroll = (trackList: string[]) => {
     const [scrollY, setScrollY] = useState(0);
+    const [scrollInfo, setScrollInfo] = useState<{ index: number; width: number }>({index: -1, width: 0});
     const trackRefs = useRef<(HTMLDivElement | null)[]>([]);
     useEffect(() => {
         const scrollEventHandler = () => {
@@ -18,9 +19,9 @@ const useTrackScroll = (trackList: string[]) => {
         return trackRefs.current.map((ele) => ele?.offsetTop);
     }, [trackRefs]);
 
-    const {newIndex: titleIndex, newWidth: soundbarWidth} = useMemo(
+    useEffect(
         () => {
-            if (trackHeights.length === 0) return {newIndex: -1, newWidth: 0};
+            if (trackHeights.length === 0) return;
             const editedScrollY = scrollY + window.innerHeight / 2;
             const getNewIndex = () => {
                 for (let i = 0; i < trackHeights.length; i++) {
@@ -36,7 +37,7 @@ const useTrackScroll = (trackList: string[]) => {
             const newWidth = newIndex === trackHeights.length - 1 ?
                 (progress * 2) * 100
                 : progress * 100;
-            return {newIndex, newWidth};
+            setScrollInfo({index: newIndex, width: newWidth});
         },
         [trackList.length, trackHeights, scrollY]
     );
@@ -53,19 +54,19 @@ const useTrackScroll = (trackList: string[]) => {
         moveToDivElement(trackRefs.current[0]);
     }, [moveToDivElement]);
     const onNextClickHandler = useCallback(() => {
-        const nextIndex = Math.min(titleIndex + 1, trackList.length - 1);
+        const nextIndex = Math.min(scrollInfo.index + 1, trackList.length - 1);
         moveToDivElement(trackRefs.current[nextIndex]);
-    }, [moveToDivElement, titleIndex, trackList.length]);
+    }, [moveToDivElement, scrollInfo.index, trackList.length]);
     const onPrevClickHandler = useCallback(() => {
-        const prevIndex = Math.max(titleIndex - 1, 0);
+        const prevIndex = Math.max(scrollInfo.index - 1, 0);
         moveToDivElement(trackRefs.current[prevIndex]);
-    }, [moveToDivElement, titleIndex]);
+    }, [moveToDivElement, scrollInfo.index]);
     const moveToDivElementByIndex = (index: number) => moveToDivElement(trackRefs.current[index]);
 
     return {
-        soundbarWidth: titleIndex < 0 ? 0 : soundbarWidth,
-        currentTitle: titleIndex < 0 ? "Loading" : trackList[titleIndex],
-        titleIndex,
+        soundbarWidth: scrollInfo.index < 0 ? 0 : scrollInfo.width,
+        currentTitle: scrollInfo.index < 0 ? "Loading" : trackList[scrollInfo.index],
+        titleIndex: scrollInfo.index,
         trackRefs,
         onPlayClickHandler,
         onNextClickHandler,
